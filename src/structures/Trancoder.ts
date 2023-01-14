5d2f0d7ad6a657df61510e3185ba5ce3e4054b89
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-01-14 12:46:59 -0300

    refactor(transcoder): improve general code and bugs

--- src/structures/Trancoder.ts
+++ src/structures/Trancoder.ts
@@ -3,6 +3,7 @@ import type { Buffer } from 'node:buffer';
 import { PassThrough, Transform } from 'node:stream';
 import prism, { type FFmpeg } from 'prism-media';
 import { type Socket, socket } from 'zeromq';
+import { sendErrorMessage } from '../utils/textChannel.js';
 import type AudioFilters from './AudioFilters.js';
 
 const { FFmpeg: FFmpegTranscoder } = prism;
@@ -10,7 +11,7 @@ const { FFmpeg: FFmpegTranscoder } = prism;
 export class AudioTransform extends Transform {
 	public audioFilters: AudioFilters;
 
-	public socket: Socket;
+	public socket: Socket | null = null;
 
 	public ffmpeg: FFmpeg;
 
@@ -35,7 +36,7 @@ export class AudioTransform extends Transform {
 				// eslint-disable-next-line no-useless-escape
 				`${filters.DynamicFilters.join(',')},azmq=bind_address=tcp\\\\://127.0.0.1\\\\:5555${
 					filters.hasFilter ? ',' : ''
-				}${filters.filters()}`,
+				}${filters.filters()},volume=1.5`,
 			],
 		});
 
@@ -45,19 +46,14 @@ export class AudioTransform extends Transform {
 			full: `${filters.DynamicFilters.join(',')},azmq=bind_address=tcp\\\\://127.0.0.1\\\\:5555,${filters.filters()}`,
 		});
 
-		this.ffmpeg.on('error', (err) => console.log(err));
-		this.ffmpeg.process.stderr?.on('data', (data) => console.log(data.toString()));
-		// this.ffmpeg.process.stdout?.on('data', (data) => console.log(data.toString()));
-
-		this.socket = socket('req');
-		// eslint-disable-next-line n/no-sync
-		this.socket.connect('tcp://127.0.0.1:5555');
-
 		this.audioFilters = filters;
 
 		this.outputStream = new PassThrough();
 
-		this.ffmpeg.process.stdout?.pipe(this.outputStream);
+		this.handleFFmpeg();
+		this.connectSocket();
+
+		this.on('close', this.onClose.bind(this));
 	}
 
 	public override _transform(
@@ -65,7 +61,6 @@ export class AudioTransform extends Transform {
 		encoding: BufferEncoding,
 		callback: (error?: Error | null, data?: Buffer) => void,
 	) {
-		console.log(chunk);
 		this.ffmpeg.process.stdin?.write(chunk, encoding);
 		callback();
 	}
@@ -78,7 +73,29 @@ export class AudioTransform extends Transform {
 		});
 
 		for (const update of this.audioFilters.socketUpdates()) {
-			this.socket.send(update, undefined, (err) => err && console.log(err));
+			this.socket?.send(update, undefined, (err) => err && console.log(err));
 		}
+	}
+
+	private connectSocket() {
+		this.socket = socket('req');
+		// eslint-disable-next-line n/no-sync
+		this.socket.connect('tcp://127.0.0.1:5555');
+	}
+
+	private handleFFmpeg() {
+		this.ffmpeg.on('error', this.onErr.bind(this));
+		this.ffmpeg.process.stderr?.on('data', this.onErr.bind(this));
+		this.ffmpeg.process.stdout?.pipe(this.outputStream, { end: false });
+	}
+
+	private onErr(data: Buffer) {
+		void sendErrorMessage(new Error(data.toString()));
+		console.error(data.toString());
+	}
+
+	private onClose() {
+		this.socket?.close();
+		this.ffmpeg.destroy();
 	}
 }
